bed0dd9e70c2633666a0565e7680d854abf1b44b
Fabricio Cabral <fabriciofx@gmail.com>
2016-10-27 08:21:33 -0300

    Adicionados os conceitos de ContaCorrente e ContaPoupanca

---
+++ src/main/java/com/github/fabriciofx/ta/banco/ContaCorrente.java
@@ -0,0 +1,5 @@
+package com.github.fabriciofx.ta.banco;
+
+public interface ContaCorrente extends Conta {
+
+}
---
+++ src/main/java/com/github/fabriciofx/ta/banco/ContaPoupanca.java
@@ -0,0 +1,5 @@
+package com.github.fabriciofx.ta.banco;
+
+public interface ContaPoupanca extends Conta {
+
+}
